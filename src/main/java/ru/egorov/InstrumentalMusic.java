cdf54058397104bde27c42ebd5a561dc76ffb9ac
Daniil Egorov <mcfirst87@gmail.com>
2022-08-04 19:04:29 +0400

    Lesson 06

---
+++ src/main/java/ru/egorov/InstrumentalMusic.java
@@ -0,0 +1,8 @@
+package ru.egorov;
+
+public class InstrumentalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Instrumental music";
+    }
+}
--- src/main/java/ru/egorov/MusicPlayer.java
+++ src/main/java/ru/egorov/MusicPlayer.java
@@ -1,13 +1,52 @@
 package ru.egorov;
 
-public class MusicPlayer {
-    private Music music;
+import java.util.ArrayList;
+import java.util.List;
 
-    public MusicPlayer(Music music){
-        this.music = music;
+public class MusicPlayer {
+    private List<Music> musicList = new ArrayList<>();
+
+    public List<Music> getMusicList() {
+        return musicList;
     }
 
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    private int volume;
+
+    /*public MusicPlayer(Music music){
+        this.music = music;
+    }*/
+    public MusicPlayer(){}
+
+    /*public void setMusic(Music music) {
+        this.music = music;
+    }*/
+
     public  void playMusic(){
-        System.out.println("Playing " + music.getSong());
+        for(Music music : musicList){
+            System.out.println("Playing " + music.getSong());
+        }
+        //System.out.println("Playing " + music.getSong());
     }
 }
