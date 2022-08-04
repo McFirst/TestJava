9725f968052e24605ad0e1aab1fcd8a98f9f80fe
Daniil Egorov <mcfirst87@gmail.com>
2022-08-04 17:48:22 +0400

    Lesson 04

---
+++ src/main/java/ru/egorov/ClassicalMusic.java
@@ -0,0 +1,9 @@
+package ru.egorov;
+
+public class ClassicalMusic implements Music {
+
+    @Override
+    public String getSong() {
+        return "Classic Music";
+    }
+}
---
+++ src/main/java/ru/egorov/RockMusic.java
@@ -0,0 +1,8 @@
+package ru.egorov;
+
+public class RockMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Rock Music";
+    }
+}
