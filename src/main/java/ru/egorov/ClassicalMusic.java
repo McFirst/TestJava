dca7ebcdbafbeda368a82cebced14c451f7f7d30
Daniil Egorov <mcfirst87@gmail.com>
2022-08-08 16:51:28 +0400

    Lesson 12

--- src/main/java/ru/egorov/ClassicalMusic.java
+++ src/main/java/ru/egorov/ClassicalMusic.java
@@ -2,14 +2,26 @@ package ru.egorov;
 
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
+//@Scope("prototype")
 public class ClassicalMusic implements Music {
+    @PostConstruct
+    public void doInit(){
+        System.out.println("Init");
+    }
+
+    @PreDestroy
+    public void doDestr(){
+        System.out.println("Destr");
+    }
     private String[] musicList = new String[] {"Classic1", "Classic2", "Classic3"};
     @Override
     public String getSong(int index) {
         return musicList[index];
     }
+
+
 }
--- src/main/java/ru/egorov/MusicPlayer.java
+++ src/main/java/ru/egorov/MusicPlayer.java
@@ -2,13 +2,26 @@ package ru.egorov;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.SecureRandom;
-import java.util.Random;
 
 @Component
 public class MusicPlayer {
+    @Value("${musicPlayer.name}")
+    private String name;
+
+    @Value("${musicPlayer.volume}")
+    private  int volume;
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
     //@Autowired
     //@Qualifier("classicalMusic")
     private Music music1;
