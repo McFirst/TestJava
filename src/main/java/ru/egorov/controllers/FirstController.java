73c9022ff7aa62957f3927ab1216b190809acd89
Daniil Egorov <mcfirst87@gmail.com>
2022-08-16 11:15:49 +0400

    Lesson 19

--- src/main/java/ru/egorov/controllers/FirstController.java
+++ src/main/java/ru/egorov/controllers/FirstController.java
@@ -3,12 +3,21 @@ package ru.egorov.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname){
+        //String name = request.getParameter("name");
+        //String surname = request.getParameter("surname");
+
+        //if((name != null) & (surname != null))
+        System.out.println("Hello " + name + " " + surname);
         return "first/hello";
     }
 
